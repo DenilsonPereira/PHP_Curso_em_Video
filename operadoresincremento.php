696e9cd1ba2dc1db3a6c7363593b726c9a55a405
DenilsonPereira <denilsongunner@gmail.com>
2022-10-14 23:06:33 -0300

    Página de estilos para os exercícios

--- operadoresincremento.php
+++ operadoresincremento.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Operadores de Incremento</title>
 </head>
 <body>
-    <h3>Mostra qual foi o ano anterior ao ano atual.</h3>
-    <?php
-        $atual = $_GET["aa"];// Essa linha vai pegar o ano na URL
-
-        echo "O ano atual é $atual e o ano interior é ". --$atual;
-
-        $c = 5; 
-
-        echo "<br>Testando C é $c e C + 1 é ". ++$c;
-    ?>
+    <main>
+        <h3>Mostra qual foi o ano anterior ao ano atual.</h3>
+        <?php
+            $atual = $_GET["aa"];// Essa linha vai pegar o ano na URL
+            echo "O ano atual é $atual e o ano interior é ". --$atual;
+            $c = 5;
+            echo "<br>Testando C é $c e C + 1 é ". ++$c;
+        ?>
+    </main>
 </body>
 </html>
