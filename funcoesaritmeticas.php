b57624439ba52c35d9d1b2c914f45ce7752e1763
DenilsonPereira <denilsongunner@gmail.com>
2022-11-02 14:24:21 -0300

    aplicando estilo

--- funcoesaritmeticas.php
+++ funcoesaritmeticas.php
@@ -4,26 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Funções Aritméticas com PHP</title>
 </head>
 <body>
-    <?php
-        $v1 = $_GET["x"];
-        $v2 = $_GET["y"];
-
-        echo "<h2>Valores recebidos: $v1 e $v2</h2>";
-
-        echo "<br> O valor absoluto de $v2 é ". abs($v2);
-
-        echo "<br> O valor de $v1<sup>$v2</sup> é ". pow($v1, $v2);
-
-        echo "<br> A raiz de $v1 é ". sqrt($v1);
-
-        echo "<br> O valor de $v2 arredondado é ". round($v2);
-
-        echo "<br> A parte inteira de $v2 é ". intval($v2);
-
-        echo "<br> O valor de $v1 em moeda é R$". number_format($v1,2, ",", ".");
-    ?>
+    <main>
+        <?php
+            $v1 = $_GET["x"];
+            $v2 = $_GET["y"];
+            echo "<h2>Valores recebidos: $v1 e $v2</h2>";
+            echo "<br> O valor absoluto de $v2 é ". abs($v2);
+            echo "<br> O valor de $v1<sup>$v2</sup> é ". pow($v1, $v2);
+            echo "<br> A raiz de $v1 é ". sqrt($v1);
+            echo "<br> O valor de $v2 arredondado é ". round($v2);
+            echo "<br> A parte inteira de $v2 é ". intval($v2);
+            echo "<br> O valor de $v1 em moeda é R$". number_format($v1,2, ",", ".");
+        ?>
+    </main>
 </body>
 </html>
--- referenciaentrevariaveis.php
+++ referenciaentrevariaveis.php
@@ -4,24 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Referência entre variáveis</title>
 </head>
 <body>
-    <?php
-        $a = 3;
-        $b = $a;
-        $b += 5;
+    <main>
+        <?php
+            $a = 3;
+            $b = $a;
+            $b += 5;
         
-        echo "A variável A vale $a";
-        echo "<br>A variável B vale $b";
-
-        echo "<p><strong>Com referência: </strong></p>";
-        $c = 3;
-        $d = &$c;
-        $d += 5;
+            echo "A variável A vale $a";
+            echo "<br>A variável B vale $b";
+            echo "<p><strong>Com referência: </strong></p>";
+            $c = 3;
+            $d = &$c;
+            $d += 5;
         
-        echo "A variável C vale $c";
-        echo "<br>A variável D vale $d";
-    ?>
+            echo "A variável C vale $c";
+            echo "<br>A variável D vale $d";
+        ?>
+    </main>
 </body>
 </html>
